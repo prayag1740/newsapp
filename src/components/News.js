edf4255d2c48c24ead1d63f3a2b898428ccb3211
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-10-15 12:29:02 +0530

    ADD : pagination in news app

--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
+const pageSize = 10
 export class News extends Component {
 
     constructor() {
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1,
+            total_results: 0
         }
     }
 
     //Lifecycle method run after render method is called ; used for loading of data
-    async componentDidMount() {
-        let url = "https://newsapi.org/v2/everything?q=india&from=2022-09-14&sortBy=publishedAt&apiKey=7d248d9c8ea64fa6a4691432a30a74ac"
+    componentDidMount() {
+        this.fetchNewsData(1) //intially fetch data for 1st page
+    }
+
+    handleNextClick = () => {
+        this.setState({page: this.state.page + 1})
+        console.log(this.state.page)
+        this.fetchNewsData(this.state.page+1)
+    }
+
+    handlePrevClick = () => {
+        this.setState({page: this.state.page - 1})
+        console.log(this.state.page)
+        this.fetchNewsData(this.state.page-1)
+    }
+
+    async fetchNewsData(page){
+        let url = `https://newsapi.org/v2/everything?q=india&from=2022-09-14&sortBy=publishedAt&apiKey=7d248d9c8ea64fa6a4691432a30a74ac&pageSize=${pageSize}&page=` + page 
         let data = await fetch(url) ;
+        console.log(data)
         let parsedData = await data.json()
-        this.setState({articles: parsedData.articles})
+        this.setState({articles: parsedData.articles, total_results: parsedData.totalResults})
+    }
+
+    showNextButton() {
+        return this.state.page + 1 > Math.ceil(this.state.total_results/pageSize)
     }
 
   render() {
@@ -30,6 +54,10 @@ export class News extends Component {
                     imageUrl={element.urlToImage} newsUrl={element.url}/>
                 </div> 
             })}
+      </div>
+      <div className='container d-flex justify-content-between'>
+      <button disabled={this.state.page <=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+      <button disabled={this.showNextButton()} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
       </div>
       </div>
     )
