1d76e40a181cc3626ca5f0119e68f3c90a8b0455
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-10-17 12:33:45 +0530

    ADD : diff news components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <NavBar />
-        <News pageSize={8} country={'in'} />
+        <Routes>
+          <Route exact path='/home' element={<News key="general" pageSize={8} country={'in'} category={'general'} />}></Route> 
+          <Route exact path='/business' element={<News key="business" pageSize={8} country={'in'} category={'business'} />}></Route>
+          <Route exact path='/entertainment' element={<News key="entertainment" pageSize={8} country={'in'} category={'entertainment'} />}></Route>
+          <Route exact path='/health' element={<News key="health" pageSize={8} country={'in'} category={'health'} />} ></Route>
+          <Route exact path='/science' element={<News key="science" pageSize={8} country={'in'} category={'science'} />}></Route>
+          <Route exact path='/sports' element={<News key='sports' pageSize={8} country={'in'} category={'sports'} />}></Route>
+          <Route exact path='/technology' element={<News key='technology' pageSize={8} country={'in'} category={'technology'} />}></Route>
+        </Routes>
+        </Router>
       </div>
+
     )
   }
 }
